81f2c0e1b66c559396cac7c06ab29859cf0ce0dd
AJIL-PAUL <ajilpaul.268@gmail.com>
2020-03-23 12:29:06 +0530

    deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const gold = require("./routes/api/gold");
@@ -68,6 +69,12 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use('/api/gold', gold);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
